11f5682aeb5a6d7b3f22728a257da032a9d00ca2
Miaomiao Chen <quic_miaochen@quicinc.com>
2024-01-02 17:09:49 +0800

    qcodec2: resolve issue for EOS and rewind case
    1. Resolve the hang issue in case below. The root cause is explained
       below.
    
       play for a while -> EOS -> Rewind -> play for a while -> EOS ...
    
    2. Reset EOS variable to support multiple EOS events.
    3. The last C2 work with EOS is not carrying any input C2 buffer,
    so that the number of pending C2 work should not be increased.
    Otherwise, after several time EOS and rewinding, the number of pending
    C2 work will be unmatched.
    4. Refine the EOS condition wait to avoid spurious wakeup.
    5. Clean up some logs.
    
    Change-Id: I1e58d15bb8d48504307e47cdfba4dfbd1663d4ac
    Signed-off-by: Miaomiao Chen <quic_miaochen@quicinc.com>

--- gst-plugin-codec2/codec2/venc/gstqcodec2venc.c
+++ gst-plugin-codec2/codec2/venc/gstqcodec2venc.c
@@ -1224,7 +1224,7 @@ static GstFlowReturn
 gst_qcodec2_venc_finish (GstVideoEncoder * encoder)
 {
   GstQcodec2Venc *enc = GST_QCODEC2_VENC (encoder);
-  gint64 timeout;
+  gint64 end_time = 0;
   BufferDescriptor inBuf;
 
   memset (&inBuf, 0, sizeof (BufferDescriptor));
@@ -1249,17 +1249,19 @@ gst_qcodec2_venc_finish (GstVideoEncoder * encoder)
   GST_VIDEO_ENCODER_STREAM_UNLOCK (encoder);
 
   g_mutex_lock (&enc->pending_lock);
-  if (!enc->eos_reached) {
+
+  end_time =
+    g_get_monotonic_time () + (EOS_WAITING_TIMEOUT * G_TIME_SPAN_SECOND);
+  while (!enc->eos_reached) {
     GST_DEBUG_OBJECT (enc, "wait until EOS signal is triggered");
 
-    timeout =
-        g_get_monotonic_time () + (EOS_WAITING_TIMEOUT * G_TIME_SPAN_SECOND);
-    if (!g_cond_wait_until (&enc->pending_cond, &enc->pending_lock, timeout)) {
+    if (!g_cond_wait_until (&enc->pending_cond, &enc->pending_lock, end_time)) {
       GST_ERROR_OBJECT (enc, "Timed out on wait, exiting!");
+      break;
     }
-  } else {
-    GST_DEBUG_OBJECT (enc, "EOS reached on output, finish encoding");
   }
+
+  enc->eos_reached = FALSE;
 
   g_mutex_unlock (&enc->pending_lock);
   GST_VIDEO_ENCODER_STREAM_LOCK (encoder);
