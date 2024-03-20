76ab35365afd79414c14797ebfeabb374424f769
Shiming Dong <quic_shimdong@quicinc.com>
2024-03-20 17:30:34 +0800

    vesdeliver: update contiguous memory heap name for kernel 6.1
    1. need to change contiguous memory heap name from qcom,display
       to system-secure for kernel 6.1
    2. secure copy tool is not supported in kernel 6.1
    
    Change-Id: I11a417793549d7e8226e7ddb25a032038997f019
    Signed-off-by: Shiming Dong <quic_shimdong@quicinc.com>

--- gst-plugin-vesdeliver/gstvesdeliver.c
+++ gst-plugin-vesdeliver/gstvesdeliver.c
@@ -268,6 +268,11 @@ gst_vesdeliver_start (GstBaseTransform * trans)
     }
 #endif
   } else if (SECURE_COPY == vesdeliver->secure) {
+#ifdef DISABLE_SECURE_COPY
+    GST_ERROR_OBJECT (vesdeliver, "secure copy mode is not supported!");
+    g_warn_if_fail (FALSE && "secure copy mode is not supported!");
+    return FALSE;
+#endif
     vesdeliver->crypto_handle = dlopen (crypto_lib_name, RTLD_NOW);
     if (NULL == vesdeliver->crypto_handle) {
       const char *dlerr = dlerror ();
