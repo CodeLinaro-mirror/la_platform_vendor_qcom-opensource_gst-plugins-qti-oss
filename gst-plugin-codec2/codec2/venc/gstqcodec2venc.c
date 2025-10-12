3b77f2c5ff462d22b067fc78cfed22a3d6a2ac37
Bo Fang <bofang@qti.qualcomm.com>
2025-10-13 04:20:40 +0800

    qcodec2venc: introduce syslog for some fatal error
    For some encoder fatal error, introduce syslog with gst log.
    Those fatal log are:
    1. input GLMemory checking error
    2. input buf type change from dma to common or common to dma
    
    Change-Id: I482efe08d365da4647322d30d65cf066f96a0b68
    Signed-off-by: Bo Fang <bofang@qti.qualcomm.com>

--- gst-plugin-codec2/codec2/venc/gstqcodec2venc.c
+++ gst-plugin-codec2/codec2/venc/gstqcodec2venc.c
@@ -74,6 +74,7 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <syslog.h>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 
@@ -2603,6 +2604,7 @@ static void gst_qcodec2_venc_check_gl_memory (GstBuffer *buf) {
       // for better performance.
       //gst_gl_memory_pbo_download_transfer ((GstGLMemoryPBO *) mem); // used in gldownload
     } else {
+      syslog (LOG_ERR, "gst-c2: " "Fatal error: mem %p is not GLMemory but has GLMemory feature", mem);
       GST_ERROR ("Fatal error: mem %p is not GLMemory but has GLMemory feature", mem);
     }
   }
@@ -2635,6 +2637,7 @@ gst_qcodec2_venc_encode (GstVideoEncoder * encoder, GstVideoCodecFrame * frame)
     if (frame->system_frame_number == 0) {
       enc->is_input_dmabuf = TRUE;
     } else if (!enc->is_input_dmabuf) {
+      syslog (LOG_ERR, "gst-c2:" "Fatal error: input buf feature changed from need copy to zero copy");
       GST_ERROR_OBJECT (enc,
           "Fatal error: input buf feature changed from need copy to zero copy");
     }
@@ -2651,6 +2654,7 @@ gst_qcodec2_venc_encode (GstVideoEncoder * encoder, GstVideoCodecFrame * frame)
     if (frame->system_frame_number == 0) {
       enc->is_input_dmabuf = FALSE;
     } else if (enc->is_input_dmabuf) {
+      syslog (LOG_ERR, "gst-c2: " "Fatal error: input buf feature changed from zero copy to need copy");
       GST_ERROR_OBJECT (enc,
           "Fatal error: input buf feature changed from zero copy to need copy");
     }
