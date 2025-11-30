945451fc706b2fd01af5c1ca9f2c349debf12423
Bo Fang <bofang@qti.qualcomm.com>
2025-11-30 21:37:48 +0800

    qcodec2: use gstc2misc.h to contain syslog/gst log macro definition
    Introduce gstc2misc.h to contain related syslog and gst log macro
    definition. Remove duplicated macro definition from gstqcodec2utils.h
    and types.h.
    
    Change-Id: I291cf6f11335ad796b3cf85abfc45754fb190bf3
    Signed-off-by: Bo Fang <bofang@qti.qualcomm.com>
    (cherry picked from commit 99e2db8a773fe1db7985dc0a88b31f425545d17c)

--- gst-plugin-codec2/codec2/wrapper/include/types.h
+++ gst-plugin-codec2/codec2/wrapper/include/types.h
@@ -28,9 +28,8 @@
 */
 
 /*
-Changes from Qualcomm Innovation Center are provided under the following license:
-
-Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+Changes from Qualcomm Technologies, Inc. are provided under the following license:
+Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
@@ -71,74 +70,7 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <glib.h>
 #include <C2Buffer.h>
 #include <gst/gst.h>
-
-
-#ifdef SLOG_TIP
-#undef SLOG_TIP
-#endif
-#ifdef SG_ERR
-#undef SG_ERR
-#endif
-#ifdef SG_ERR_OBJ
-#undef SG_ERR_OBJ
-#endif
-#ifdef SG_INFO
-#undef SG_INFO
-#endif
-#ifdef SG_INFO_OBJ
-#undef SG_INFO_OBJ
-#endif
-#ifdef ENABLE_GSTC2_DEFAULT_SYSLOG
-#undef ENABLE_GSTC2_DEFAULT_SYSLOG
-#endif
-
-#define ENABLE_GSTC2_DEFAULT_SYSLOG
-#ifndef ENABLE_GSTC2_DEFAULT_SYSLOG
-#define SG_ERR        GST_ERROR
-#define SG_ERR_OBJ    GST_ERROR_OBJECT
-#define SG_INFO       GST_INFO
-#define SG_INFO_OBJ   GST_INFO_OBJECT
-#else
-#include <syslog.h>
-#define SLOG_TIP "gst-c2:"
-//if fmt contain GST specific format like GST_PTR_FORMAT/GST_SEGMENT_FORMAT or fmt is var, must use gst_info_strdup_printf() to explain those GST specific format and var
-#define SG_ERR(fmt, args...)                                   \
-    do {                                                       \
-        gchar* __sg_str = gst_info_strdup_printf(fmt, ##args); \
-        if (__sg_str) {                                        \
-            syslog(LOG_ERR, SLOG_TIP "E: %s", __sg_str);       \
-            g_free(__sg_str);                                  \
-        }                                                      \
-        GST_ERROR(fmt, ##args);                                \
-    } while(0)
-#define SG_ERR_OBJ(obj, fmt, args...)                          \
-    do {                                                       \
-        gchar* __sg_str = gst_info_strdup_printf(fmt, ##args); \
-        if (__sg_str) {                                        \
-            syslog(LOG_ERR, SLOG_TIP "E: %s", __sg_str);       \
-            g_free(__sg_str);                                  \
-        }                                                      \
-        GST_ERROR_OBJECT(obj, fmt, ##args);                    \
-    } while(0)
-#define SG_INFO(fmt, args...)                                  \
-    do {                                                       \
-        gchar* __sg_str = gst_info_strdup_printf(fmt, ##args); \
-        if (__sg_str) {                                        \
-            syslog(LOG_NOTICE, SLOG_TIP "I: %s", __sg_str);    \
-            g_free(__sg_str);                                  \
-        }                                                      \
-        GST_INFO(fmt, ##args);                                 \
-    } while(0)
-#define SG_INFO_OBJ(obj, fmt, args...)                         \
-    do {                                                       \
-        gchar* __sg_str = gst_info_strdup_printf(fmt, ##args); \
-        if (__sg_str) {                                        \
-            syslog(LOG_NOTICE, SLOG_TIP "I: %s", __sg_str);    \
-            g_free(__sg_str);                                  \
-        }                                                      \
-        GST_INFO_OBJECT(obj, fmt, ##args);                     \
-    } while(0)
-#endif
+#include <gstc2misc.h>
 
 #define UNUSED(x) (void)(x)
 
